792be7e0bf2babe55f56ac7e386b20e6288a0434
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2019-09-22 11:56:37 +0700

    add board blog admin

--- src/app/Admin/blog-list/blog-listAdmin.component.ts
+++ src/app/Admin/blog-list/blog-listAdmin.component.ts
@@ -1,11 +1,47 @@
-import {Component} from '@angular/core'
+import { Component, AfterViewInit, OnInit, Injectable } from '@angular/core'
+import { BlogServices } from '../../services/blog.service'
+import { UserService } from '../../services/user.service'
+import * as API from '../../services/config'
 
+@Injectable()
 @Component({
     selector: 'blog-list-admin',
     styleUrls: ['./blog-listAdmin.component.css'],
     templateUrl: './blog-listAdmin.component.html'
 })
 
-export class BlogListAdmin{
-    
+export class BlogListAdmin implements OnInit, AfterViewInit {
+    ListBlog: []
+    linkServerIMG: String
+
+    constructor(private _blogService: BlogServices,private _userService : UserService) { }
+
+    ngOnInit(): void {
+        this.linkServerIMG = `${API.urlServer}/assert/images`
+    }
+    ngAfterViewInit(): void {
+        this._blogService.getList().subscribe(
+            response => {
+                let status = response['status']
+                let listBlog = response['listBlog']
+                let message = response['message']
+                if (listBlog.length > 0 && status === 200) {
+                    listBlog.map(item => {
+                        if (item.picture !== null) {
+                            item.picture = `${this.linkServerIMG}/${item.picture}`
+                            return item
+                        }
+                        return item
+                    })
+                    this.ListBlog = listBlog
+                }else
+                if (status === 403) {
+                    this._userService.Logout()
+                } else {
+                    this._userService.Logout()
+                }
+            },
+            error => { console.log(error) }
+        )
+    }
 }
